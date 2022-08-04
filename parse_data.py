33fd165f4eaa16966be3883d9b507f9a491998af
Luke Williams <lukeawilliams@gmail.com>
2022-08-04 14:05:00 -0500

    Initial commit

---
+++ parse_data.py
@@ -0,0 +1,136 @@
+import sqlite3
+import csv
+import urllib.request
+import zipfile
+
+gmrs_URL = "https://data.fcc.gov/download/pub/uls/complete/l_gmrs.zip"
+gmrs_ZIP = "./data/l_gmrs.zip"
+amat_URL = "https://data.fcc.gov/download/pub/uls/complete/l_amat.zip"
+amat_ZIP = "./data/l_amat.zip"
+
+gmrs_dir = "./data/l_gmrs/" 
+gmrs_HD = "./data/l_gmrs/HD.dat"
+gmrs_EN = "./data/l_gmrs/EN.dat"
+
+amat_dir = "./data/l_amat/"
+amat_HD = "./data/l_amat/HD.dat"
+amat_EN = "./data/l_amat/EN.dat"
+amat_AM = "./data/l_amat/AM.dat"
+
+DB_filepath = "callsign.sqlite3"
+
+print("Downloading GMRS ZIP from FCC")
+urllib.request.urlretrieve(gmrs_URL, gmrs_ZIP)
+print("Downloading amateur ZIP from FCC")
+urllib.request.urlretrieve(amat_URL, amat_ZIP)
+
+print("Extracting GMRS files from ZIP")
+with zipfile.ZipFile(gmrs_ZIP, 'r') as zip_ref:
+    zip_ref.extract("HD.dat", gmrs_dir)
+    zip_ref.extract("EN.dat", gmrs_dir)
+
+print("Extracting amateur files from ZIP")
+with zipfile.ZipFile(amat_ZIP, 'r') as zip_ref:
+    zip_ref.extract("HD.dat", amat_dir)
+    zip_ref.extract("EN.dat", amat_dir)
+    zip_ref.extract("AM.dat", amat_dir)
+
+print("Creating SQLite DB in memory")
+con = sqlite3.connect(':memory:')
+cur = con.cursor()
+cur.execute("CREATE TABLE hd (usid, callsign, status, service)")
+cur.execute("CREATE TABLE en (usid, name, street, city, state, zip, frn)")
+cur.execute("CREATE TABLE am (usid, class, prevcall)")
+
+csv.register_dialect('piper', delimiter='|', quoting=csv.QUOTE_NONE)
+
+print("Importing GMRS")
+print("  Processing HD")
+with open(gmrs_HD, "r") as csvfile:
+    good_lines = 0
+    bad_lines = 0
+    for row in csv.reader(csvfile, dialect='piper'):
+        if len(row) == 59 and row[0] == "HD" and row[5] == 'A':
+            cur.execute("INSERT INTO hd VALUES (?, ?, ?, ?)", (row[1], row[4], row[5], row[6]))
+            good_lines += 1
+        else:
+            bad_lines += 1
+print("    " + str(good_lines) + " / " + str(bad_lines))
+            
+print("  Bulk EN")
+with open(gmrs_EN, "r") as csvfile:
+    good_lines = 0
+    bad_lines = 0
+    for row in csv.reader(csvfile, dialect='piper'):
+        if len(row) == 30 and row[0] == "EN":
+            cur.execute("INSERT INTO en VALUES (?, ?, ?, ?, ?, ?, ?)", (row[1], row[7], row[15], row[16], row[17], row[18], row[22]))
+            good_lines += 1
+        else:
+            bad_lines += 1
+print("    " + str(good_lines) + " / " + str(bad_lines))
+
+print("Importing Amateur")
+print("  Processing HD")
+with open(amat_HD, "r") as csvfile:
+    good_lines = 0
+    bad_lines = 0
+    for row in csv.reader(csvfile, dialect='piper'):
+        if len(row) == 59 and row[0] == "HD" and row[5] == 'A':
+            cur.execute("INSERT INTO hd VALUES (?, ?, ?, ?)", (row[1], row[4], row[5], row[6]))
+            good_lines += 1
+        else:
+            bad_lines += 1
+print("    " + str(good_lines) + " / " + str(bad_lines))
+
+print("  Bulk EN")
+with open(amat_EN, "r") as csvfile:
+    good_lines = 0
+    bad_lines = 0
+    for row in csv.reader(csvfile, dialect='piper'):
+        if len(row) == 30 and row[0] == "EN":
+            cur.execute("INSERT INTO en VALUES (?, ?, ?, ?, ?, ?, ?)", (row[1], row[7], row[15], row[16], row[17], row[18], row[22]))
+            good_lines += 1
+        else:
+            bad_lines += 1
+print("    " + str(good_lines) + " / " + str(bad_lines))
+
+print("  Bulk AM")
+with open(amat_AM, "r") as csvfile:
+    good_lines = 0
+    bad_lines = 0
+    for row in csv.reader(csvfile, dialect='piper'):
+        if len(row) == 18 and row[0] == "AM":
+            cur.execute("INSERT INTO am VALUES (?, ?, ?)", (row[1], row[5], row[15]))
+            good_lines += 1
+        else:
+            bad_lines += 1
+print("    " + str(good_lines) + " / " + str(bad_lines))
+
+print("Exporting data from SQLite in memory to", DB_filepath)
+con.execute("ATTACH DATABASE '" + DB_filepath + "' AS clean;").fetchall()
+print("  Inserting table into", DB_filepath)
+con.execute("CREATE TABLE clean.licenses AS SELECT hd.usid,callsign,status,service,name,street,city,state,zip,frn,class,prevcall FROM hd INNER JOIN en ON hd.usid = en.usid LEFT JOIN am ON hd.usid = am.usid;").fetchall()
+print("  Cleaning up")
+con.execute("COMMIT;").fetchall()
+con.execute("DETACH clean;").fetchall()
+con.commit()
+con.close()
+
+print("Optimizing DB")
+print("  Connecting")
+con = sqlite3.connect(DB_filepath)
+
+print("  Building indices")
+con.execute("CREATE INDEX callsign ON licenses (callsign);").fetchall()
+con.execute("CREATE INDEX frn ON licenses (frn);").fetchall()
+con.execute("pragma journal_mode = delete;").fetchall()
+con.execute("pragma page_size = 1024;").fetchall()
+
+print("  Vacuum")
+con.execute("vacuum;").fetchall()
+
+print("  Cleaning up")
+con.commit()
+con.close()
+
+print("Done")
---
+++ src/index.ts
@@ -0,0 +1,78 @@
+import { createDbWorker } from "sql.js-httpvfs";
+
+const workerUrl = new URL(
+  "sql.js-httpvfs/dist/sqlite.worker.js",
+  import.meta.url
+);
+const wasmUrl = new URL("sql.js-httpvfs/dist/sql-wasm.wasm", import.meta.url);
+
+async function load() {
+  const worker = await createDbWorker(
+    [
+      {
+        from : "jsonconfig",
+        configUrl : "/config.json"
+      },
+    ],
+    workerUrl.toString(),
+    wasmUrl.toString()
+  );
+
+  var queryString = window.location.search;
+  console.log("qs-search:", queryString.search(/^\?[a-zA-Z0-9]{1,7}$/))
+  if(queryString.search(/^\?[a-zA-Z0-9]{1,7}$/) == 0){
+    var callsign = queryString.toUpperCase().replace("?","")
+    console.log("Callsign:",callsign)
+    const frnlookup : any  = await worker.db.query(`select frn from licenses WHERE callsign = ?`, [callsign]);
+    if(frnlookup.length > 0){
+        const frn = frnlookup[0]['frn']
+        console.log("FRN: ", frn)
+        const licenses : any = await worker.db.query(`select * from licenses WHERE frn = ?`, [frn]);
+        console.log(JSON.stringify(licenses, null, 2))
+        if(licenses.length > 0){
+            const results = document.getElementById("results")
+            if(results) {
+                licenses.forEach(function(item : any){
+                    var resultDiv = document.createElement('div');
+                    resultDiv.className = 'result'
+                    results.appendChild(resultDiv);
+
+                    var callDiv = document.createElement('div');
+                    callDiv.className = 'callsign'
+                    callDiv.textContent = item.callsign
+                    resultDiv.appendChild(callDiv);
+
+                    var nameDiv = document.createElement('div');
+                    nameDiv.className = 'name'
+                    nameDiv.textContent = item.name
+                    resultDiv.appendChild(nameDiv);
+
+                    var addressDiv = document.createElement('div');
+                    addressDiv.className = 'address'
+                    addressDiv.textContent = item.street
+                    resultDiv.appendChild(addressDiv);
+
+                    var cityDiv = document.createElement('div');
+                    cityDiv.className = 'address'
+                    cityDiv.textContent += item.city + ', ' + item.state + ' ' + item.zip
+                    resultDiv.appendChild(cityDiv);
+
+                    var serviceDiv = document.createElement('div');
+                    serviceDiv.className = 'service'
+                    serviceDiv.textContent += item.service
+                    if(item.class){serviceDiv.textContent += ' ' + item.class}
+                    if(item.prevcall){serviceDiv.textContent += ' ' + item.prevcall}
+                    resultDiv.appendChild(serviceDiv);
+
+                })
+            }
+        }
+    } else {
+        const results = document.getElementById("results")
+        if(results){results.textContent = "No license found.";}
+        console.log("No FRN found")
+    }
+  }
+}
+
+load();
